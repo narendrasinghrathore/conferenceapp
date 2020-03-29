191ade80906ef36e809aa6865e4060e9eafbd63e
narendrasinghrathore <rathoreofficialemail@gmail.com>
2020-03-29 19:29:30 +0530

    Bind data on item click

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,4 +1,8 @@
 import { LightningElement, track } from 'lwc';
 export default class App extends LightningElement {
     @track sessionId;
+
+    handleNavigate(event) {
+        this.sessionId = event.detail;
+    }
 }
